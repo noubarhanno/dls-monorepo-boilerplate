4677bf7061837c56167cecb8832a1c15ff49746a
Noubar Jack <noubar@kmmrce.com>
2023-02-01 09:45:11 +0400

    chore(setup): semantic release with configuration

---
+++ packages/core/release.config.js
@@ -0,0 +1,29 @@
+const name = 'core';
+const srcRoot = `packages/${name}`;
+
+module.exports = {
+  extends: `release.config.base.js`,
+  pkgRoot: `${srcRoot}`,
+  npmPublish: false,
+  tagFormat: name + '-v${version}',
+  commitPaths: [`${srcRoot}/*`],
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: `${srcRoot}/CHANGELOG.md`,
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: [`${srcRoot}/CHANGELOG.md`, `${srcRoot}/package.json`],
+        message: `chore(release): ${name} v\${nextRelease.version} [skip ci]`,
+      },
+    ],
+  ],
+};
