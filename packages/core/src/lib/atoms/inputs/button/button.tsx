cf89aeb0786c9002a947494b889f88e98052ea32
Noubar Jack <noubar@kmmrce.com>
2023-02-03 14:55:29 +0400

    fix(styles): button to pass ui test on chromatic

--- packages/core/src/lib/atoms/inputs/button/button.tsx
+++ packages/core/src/lib/atoms/inputs/button/button.tsx
@@ -6,7 +6,7 @@ import styled from '@emotion/styled';
 export interface ButtonProps {}
 
 const StyledButton = styled.button`
-  color: red;
+  color: green;
   padding: 6px;
   border: 1px solid black;
 `;
